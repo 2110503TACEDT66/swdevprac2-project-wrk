7d803bca71857c5758d987c6530b88a8e0a47024
Ramida_Soisrikham <6633205621@student.chula.ac.th>
2024-03-26 04:03:24 +0700

    try resolve for Vercel

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,10 +3,15 @@ import {AuthOptions} from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import userLogin from '@/libs/userLogin';
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
 	providers: [
 		CredentialsProvider({
+			// The name to display on the sign in form (e.g. "Sign in with...")
 			name: 'Credentials',
+			// `credentials` is used to generate a form on the sign in page.
+			// You can specify which fields should be submitted, by adding keys to the `credentials` object.
+			// e.g. domain, username, password, 2FA token, etc.
+			// You can pass any HTML attribute to the <input> tag through the object.
 			credentials: {
 				email: {label: 'Email', type: 'email', placeholder: 'email'},
 				password: {
@@ -20,9 +25,13 @@ export default NextAuth({
 				const user = await userLogin(credentials.email, credentials.password);
 
 				if (user) {
+					// Any object returned will be saved in `user` property of the JWT
 					return user;
 				} else {
+					// If you return null then an error will be displayed advising the user to check their details.
 					return null;
+
+					// You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
 				}
 			},
 		}),
@@ -37,4 +46,6 @@ export default NextAuth({
 			return session;
 		},
 	},
-});
+};
+const handler = NextAuth(authOptions);
+export {handler as GET, handler as POST};
