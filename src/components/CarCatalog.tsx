d31bc9fc6eef76ae8c8355eade069be657cffbba
PuthEmplankess <puthsoisrikhum@gmail.com>
2024-03-26 20:27:27 +0700

    add Interface

--- src/components/CarCatalog.tsx
+++ src/components/CarCatalog.tsx
@@ -1,12 +1,58 @@
 import ProductCard from './ProductCard';
 import Link from 'next/link';
 
-export default async function CarCatalog({carJson}: {carJson: object}) {
-	const carJsonRedy = await carJson;
+interface coworkObj {
+	success: boolean;
+	count: number;
+	pagination: Record<string, any>; // Since the structure is unknown
+	data: coworkItem[];
+}
+
+interface coworkItem {
+	success: boolean;
+	count: number;
+	pagination: Record<string, any>; // Since the structure is unknown
+	data: any[];
+}
+interface reservationItem {
+	_id: string;
+	user: string;
+	coWork: string;
+	date: string;
+	startTime: string;
+	endTime: string;
+	table: string;
+	createdAt: string;
+	__v: number;
+}
+
+interface coworkItem {
+	_id: string;
+	name: string;
+	address: string;
+	district: string;
+	province: string;
+	postalcode: string;
+	region: string;
+	tel: string;
+	Open_time: string;
+	Close_time: string;
+	picture: string;
+	__v: number;
+	reservations: reservationItem[];
+	id: string;
+}
+
+export default async function CarCatalog({
+	coworkJson,
+}: {
+	coworkJson: coworkObj;
+}) {
+	const coworkJsonRedy = await coworkJson;
 
 	return (
 		<>
-			Explore {carJsonRedy.count} co-working space in our catalog
+			Explore {coworkJsonRedy.count} co-working space in our catalog
 			<div
 				style={{
 					margin: '20px',
@@ -17,9 +63,12 @@ export default async function CarCatalog({carJson}: {carJson: object}) {
 					alignContent: 'space-around',
 				}}
 			>
-				{carJsonRedy.data.map((carItem: Object) => (
-					<Link href={`/car/${carItem.id}`} className="w-1/5">
-						<ProductCard carName={carItem.name} imgSrc={carItem.picture} />
+				{coworkJsonRedy.data.map((coworkItem: coworkItem) => (
+					<Link href={`/car/${coworkItem.id}`} className="w-1/5">
+						<ProductCard
+							carName={coworkItem.name}
+							imgSrc={coworkItem.picture}
+						/>
 					</Link>
 				))}
 			</div>
