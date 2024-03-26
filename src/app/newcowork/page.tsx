5e4b4ca55b23ec9aa211029e364e5393db40b1cb
PuthEmplankess <puthsoisrikhum@gmail.com>
2024-03-27 02:17:40 +0700

    initial Admin management

--- src/app/newcowork/page.tsx
+++ src/app/newcowork/page.tsx
@@ -5,159 +5,140 @@ import router from 'next/router';
 import Link from 'next/link';
 
 export default function RegisterPage() {
-	const [formData, setFormData] = useState({
-		name: '',
-		address: '',
-		district: '',
-		province: '',
-		postalcode: '',
-		region: '',
-		tel: '',
-		opentime: '',
-		closetime: '',
-		picture: '',
-	});
-
-	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-		const {name, value} = e.target;
-		setFormData((prevData) => ({
-			...prevData,
-			[name]: value,
-		}));
-	};
-
-	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		// Perform registration logic here, such as sending data to backend
-		console.log(formData); // For demonstration, you can replace this with your registration logic
-	};
-
 	return (
-		<div className={styles.container}>
-			{' '}
-			{/* Apply container class for centering */}
-			<form onSubmit={handleSubmit} className={styles.form}>
-				{' '}
-				{/* Apply form class for styling */}
-				<div className={styles.formGroup}>
+		<form className="my-[10%] mx-[25%] w-[80%] h-[60%]">
+			<div className="text-xl text-blue-700">Add New Co-Working Space</div>
+			<div className="flex items-center w-1/2 my-2 ">
+				<label className="w-auto block text-gray-700 pr-4" htmlFor="model">
 					{' '}
-					{/* Apply formGroup class for styling */}
-					<label htmlFor="name">Name:</label>
-					<input
-						type="text"
-						id="name"
-						name="name"
-						value={formData.name}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="address">Address:</label>
-					<input
-						type="text"
-						id="address"
-						name="address"
-						value={formData.address}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="district">District:</label>
-					<input
-						type="text"
-						id="district"
-						name="district"
-						value={formData.district}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="province">Province:</label>
-					<input
-						type="text"
-						id="province"
-						name="province"
-						value={formData.province}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="postalcode">Postalcode:</label>
-					<input
-						type="text"
-						id="postalcode"
-						name="postalcode"
-						value={formData.postalcode}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="region">region:</label>
-					<input
-						type="text"
-						id="region"
-						name="region"
-						value={formData.region}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="tel">Tel:</label>
-					<input
-						type="tel"
-						id="tel"
-						name="tel"
-						value={formData.tel}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="opentime">Opentime:</label>
-					<input
-						type="time"
-						id="opentime"
-						name="opentime"
-						value={formData.opentime}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="closetime">Closetime:</label>
-					<input
-						type="time"
-						id="closetime"
-						name="closetime"
-						value={formData.closetime}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className={styles.formGroup}>
-					<label htmlFor="picture">picture:</label>
-					<input
-						type="text"
-						id="picture"
-						name="picture"
-						value={formData.picture}
-						onChange={handleChange}
-						required
-					/>
-				</div>
-				<Link href="/car">
-					<button type="submit" className={styles.submitButton}>
-						Add New CoWork
-					</button>
-				</Link>
-				{/* Apply submitButton class for styling */}
-			</form>
-		</div>
+					Model
+				</label>
+				<input
+					type="text"
+					required
+					id="model"
+					name="model"
+					placeholder="Car Model"
+					className="bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus: outline-none focus: border-blue-400"
+				/>
+			</div>
+			<div className="flex items-center w-1/2 my-2">
+				<label className="w-auto block text-gray-700 pr-4" htmlFor="desc">
+					Description
+				</label>
+				<input
+					type="text"
+					required
+					id="desc"
+					name="desc"
+					placeholder="Car Description"
+					className="bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus: outline-none focus: border-blue-400"
+				/>
+			</div>
+			<div className="flex items-center w-1/2 my-2">
+				<label className="w-auto block text-gray-700 pr-4" htmlFor="picture">
+					{' '}
+					Picture
+				</label>
+				<input
+					type="text"
+					required
+					id="picture"
+					name="picture"
+					placeholder="URL"
+					className="bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus: outline-none focus: border-blue-400"
+				/>
+			</div>
+			<div className="flex items-center w-1/2 my-2">
+				<label className="w-auto block text-gray-700 pr-4" htmlFor="seats">
+					{' '}
+					Seats
+				</label>
+				<input
+					type="number"
+					required
+					id="seats"
+					name="seats"
+					placeholder="4"
+					min={0}
+					max={50}
+					className="bg-white border-2 border-gray-200 rounded w-auto p-2 text-gray-700 focus: outline-none focus: border-blue-400"
+				/>
+				<label className="w-auto block text-gray-700 pr-4 m1-5" htmlFor="doors">
+					{' '}
+					Doors
+				</label>
+				<input
+					type="number"
+					required
+					id="doors"
+					name="doors"
+					placeholder="4"
+					min={0}
+					max={8}
+					className="bg-white border-2 border-gray-200 rounded w-auto p-2 text-gray-700 focus: outline-none focus: border-blue-400"
+				/>
+				<input
+					className="ml-5 mr-2"
+					id="automatic"
+					name="automatic"
+					type="checkbox"
+				/>{' '}
+				<span>Auto</span>
+			</div>
+			<div className="flex items-center w-1/2 my-2">
+				<label className="w-auto block text-gray-700 pr-4" htmlFor="largebags">
+					{' '}
+					Large Bags
+				</label>
+				<input
+					type="number"
+					required
+					id="largebags"
+					name="largebags"
+					placeholder="2"
+					min={0}
+					max={10}
+					className="bg-white border-2 border-gray-200 rounded w-auto p-2 text-gray-700 focus: outline-none focus: border-blue-400"
+				/>
+				<label
+					className="w-auto block text-gray-700 pr-4 ml-5"
+					htmlFor="smallbags"
+				>
+					{' '}
+					Small Bags
+				</label>
+				<input
+					type="number"
+					required
+					id="smallbags"
+					name="smallbags"
+					placeholder="2"
+					min={0}
+					max={10}
+					className="bg-white border-2 border-gray-200 rounded w-auto p-2 text-gray-700 focus: outline-none focus: border-blue-400"
+				/>
+			</div>
+			<div className="flex items-center w-1/2 my-2">
+				<label className="w-auto block text-gray-700 pr-4" htmlFor="dayRate">
+					{' '}
+					Rate
+				</label>
+				<input
+					type="text"
+					required
+					id="dayRate"
+					name="dayRate"
+					placeholder="Daily Rate (including insurance)"
+					className="bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus: outline-none focus: border-blue-400"
+				/>
+			</div>
+			<button
+				type="submit"
+				className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+			>
+				Add New Car
+			</button>
+		</form>
 	);
 }
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -4,10 +4,27 @@ import TopMenuItem from './TopMenuItem';
 import {getServerSession} from 'next-auth';
 import {authOptions} from '@/app/api/auth/[...nextauth]/route';
 import {Link} from '@mui/material';
+import getUserProfile from '@/libs/getUserProfile';
 
 export default async function TopMenu() {
 	const session = await getServerSession(authOptions);
-
+	if (!session || !session.user.token)
+		return (
+			<div className={styles.menucontainer}>
+				<Link href="/">
+					<Image
+						src={'/img/logo.png'}
+						className={styles.logoimg}
+						alt="logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+					/>
+				</Link>
+				<TopMenuItem title="Reservation" pageRef="/cart" />
+			</div>
+		);
+	const proflie = await getUserProfile(session.user.token);
 	return (
 		<div className={styles.menucontainer}>
 			<Link href="/">
@@ -21,7 +38,9 @@ export default async function TopMenu() {
 				/>
 			</Link>
 			<TopMenuItem title="Reservation" pageRef="/cart" />
-			<TopMenuItem title="About" pageRef="/about" />
+			{proflie.data.role === 'admin' ? (
+				<TopMenuItem title="Admin" pageRef="/reservations/manage" />
+			) : null}
 			{session ? (
 				<Link href="/api/auth/signout">
 					<div className="flex items-center absolute right-0  h-full px-2 text-cyan-600 text-sm">
