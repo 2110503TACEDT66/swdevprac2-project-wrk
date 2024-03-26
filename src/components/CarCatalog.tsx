8d11497c352be53e7849736d37bbb9f1b1ffa871
PuthEmplankess <puthsoisrikhum@gmail.com>
2024-03-26 19:42:15 +0700

    try fix 'not exist on type 'object'.'

--- src/components/CarCatalog.tsx
+++ src/components/CarCatalog.tsx
@@ -1,12 +1,48 @@
 import ProductCard from './ProductCard';
 import Link from 'next/link';
 
-export default async function CarCatalog({carJson}: {carJson: object}) {
-	const carJsonRedy = await carJson;
+interface Reservation {
+	_id: string;
+	user: string;
+	coWork: string;
+	date: string;
+	startTime: string;
+	endTime: string;
+	table: string;
+	createdAt: string;
+	__v: number;
+}
+
+interface CoworkingSpace {
+	_id: string;
+	name: string;
+	address: string;
+	district: string;
+	province: string;
+	postalcode: string;
+	region: string;
+	tel: string;
+	Open_time: string;
+	Close_time: string;
+	picture: string;
+	__v: number;
+	reservations: Reservation[];
+	id: string;
+}
+
+interface CarJson {
+	success: boolean;
+	count: number;
+	pagination: Record<string, any>; // Since the structure is unknown
+	data: CoworkingSpace[];
+}
+
+export default async function CarCatalog({carJson}: {carJson: CarJson}) {
+	const {count, data} = carJson;
 
 	return (
 		<>
-			Explore {carJsonRedy.count} co-working space in our catalog
+			Explore {count} co-working spaces in our catalog
 			<div
 				style={{
 					margin: '20px',
@@ -17,8 +53,8 @@ export default async function CarCatalog({carJson}: {carJson: object}) {
 					alignContent: 'space-around',
 				}}
 			>
-				{carJsonRedy.data.map((carItem: Object) => (
-					<Link href={`/car/${carItem.id}`} className="w-1/5">
+				{data.map((carItem) => (
+					<Link key={carItem.id} href={`/car/${carItem.id}`} className="w-1/5">
 						<ProductCard carName={carItem.name} imgSrc={carItem.picture} />
 					</Link>
 				))}
