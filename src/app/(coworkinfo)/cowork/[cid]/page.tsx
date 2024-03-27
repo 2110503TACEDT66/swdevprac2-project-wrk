990381d1f117a0ea18146a69d2c5bbdd3f4b761c
PuthEmplankess <puthsoisrikhum@gmail.com>
2024-03-27 09:24:08 +0700

    edit CoworkCard

--- src/app/(coworkinfo)/cowork/[cid]/page.tsx
+++ src/app/(coworkinfo)/cowork/[cid]/page.tsx
@@ -4,9 +4,13 @@ import Link from 'next/link';
 import {getServerSession} from 'next-auth';
 import {authOptions} from '@/app/api/auth/[...nextauth]/route';
 import getUserProfile from '@/libs/getUserProfile';
-import UpdateCoWork from '@/components/UpdateCoWork'
+import UpdateCoWork from '@/components/UpdateCoWork';
 
-export default async function CoworkDetailPage({params}: {params: {cid: string}}) {
+export default async function CoworkDetailPage({
+	params,
+}: {
+	params: {cid: string};
+}) {
 	const CoworkDetail = await getCowork(params.cid);
 
 	const session = await getServerSession(authOptions);
@@ -15,40 +19,48 @@ export default async function CoworkDetailPage({params}: {params: {cid: string}}
 	const proflie = await getUserProfile(session.user.token);
 	return (
 		<main className="text-center p-5">
-			<h1 className="text-lg font-medium">{CoworkDetail.data.name}</h1>
-			<div className="flex flex-row my-5">
+			<h1 className="text-lg font-medium ">{CoworkDetail.data.name}</h1>
+			<div className="flex flex-row my-5 flex-wrap ml-[15%] w-[100%]">
 				<Image
 					src={CoworkDetail.data.picture}
-					alt="Car Image"
+					alt="CoWork Image"
 					width={0}
 					height={0}
 					sizes="100vw"
 					className="rounded-lg w-[30%]"
 				/>
-				<div className="text-md mx-5 text-left">
-					{CoworkDetail.data.address}
-					<div className="text-md mx-5">{CoworkDetail.data.district}</div>
-					<div className="text-md mx-5">{CoworkDetail.data.region}</div>
-					<div className="text-md mx-5">
+				<div className="text-md mx-5 text-left m-5 ml-[5%] w-[50%]">
+					<div className="text-md m-5">
+						Location : {CoworkDetail.data.address}
+					</div>
+
+					<div className="text-md m-5">
+						District : {CoworkDetail.data.district}
+					</div>
+					<div className="text-md m-5">Region : {CoworkDetail.data.region}</div>
+					<div className="text-md m-5">
 						Open time: {CoworkDetail.data.Open_time}
 					</div>
-					<div className="text-md mx-5">
-						Close time:{CoworkDetail.data.Close_time}
+					<div className="text-md m-5">
+						Close time: {CoworkDetail.data.Close_time}
 					</div>
-					<div className="text-md mx-5">Tel: {CoworkDetail.data.tel}</div>
+					<div className="text-md m-5">Tel: {CoworkDetail.data.tel}</div>
 
-					<Link href={`/reservations?id=${params.cid}&name=${CoworkDetail.data.name}`}>
-                    <button className="block rounded-md bg-orange-400 hover:bg-orange-600 px-3 py-1
-                    text-white shadow-sm">
-                        Make a Reservation
-                    </button>
-                </Link>
+					<Link
+						href={`/reservations?id=${params.cid}&name=${CoworkDetail.data.name}`}
+					>
+						<button
+							className="block rounded-md bg-orange-400 hover:bg-orange-600 px-3 py-1
+                    text-white shadow-sm m-5"
+						>
+							Make a Reservation
+						</button>
+					</Link>
+					{proflie.data.role === 'admin' ? (
+						<UpdateCoWork CoworkDetail={CoworkDetail} params={params} />
+					) : null}
 				</div>
 			</div>
-			{
-				(proflie.data.role === 'admin')?
-				<UpdateCoWork CoworkDetail ={CoworkDetail} params={params} />: null
-			}
 		</main>
 	);
 }
