a373ba71e5e76f19890e60cb705f1f7f67c64a92
PuthEmplankess <puthsoisrikhum@gmail.com>
2024-03-31 22:18:08 +0700

    Final Deploy

--- src/libs/userRegister.tsx
+++ src/libs/userRegister.tsx
@@ -4,18 +4,21 @@ export default async function userRegister(
 	userPassword: string,
 	userTel: string
 ) {
-	const response = await fetch('http://localhost:5050/api/v1/auth/register', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			name: userName,
-			email: userEmail,
-			password: userPassword,
-			tel: userTel,
-		}),
-	});
+	const response = await fetch(
+		`${process.env.BACKEND_URL}/api/v1/auth/register`,
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name: userName,
+				email: userEmail,
+				password: userPassword,
+				tel: userTel,
+			}),
+		}
+	);
 
 	if (!response.ok) {
 		throw new Error('Failed to Register');
